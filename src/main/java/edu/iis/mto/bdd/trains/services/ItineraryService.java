24d888dce466368f7c0347431dfcc62dee506438
Marcin Kapelan <marcinkape@gmail.com>
2018-09-12 18:59:51 +0200

    feature1_impl Create ItineraryService and assertion in @Then annotated method of OptimalItinerarySteps class

---
+++ src/main/java/edu/iis/mto/bdd/trains/services/ItineraryService.java
@@ -0,0 +1,11 @@
+package edu.iis.mto.bdd.trains.services;
+
+import org.joda.time.LocalTime;
+import java.util.List;
+
+public class ItineraryService {
+
+    public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime) {
+        return null;
+    }
+}
--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,8 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import edu.iis.mto.bdd.trains.services.ItineraryService;
+import org.hamcrest.Matchers;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -10,7 +12,12 @@ import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class OptimalItinerarySteps {
+
+    private ItineraryService itineraryService = new ItineraryService();
+    private List<LocalTime> trainTimes;
 
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
@@ -22,11 +29,11 @@ public class OptimalItinerarySteps {
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-        throw new PendingException();
+        trainTimes = itineraryService.findNextDepartures(departure, destination, startTime);
     }
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        throw new PendingException();
+        assertThat(trainTimes, Matchers.is(expectedTrainTimes));
     }
 }
